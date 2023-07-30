7d7e53dcd763f0678874d8e9961717412eb15d92
dreamjser <dreamjser@gmail.com>
2023-07-30 23:53:30 +0800

    vue desktop 界面库

--- template/vuedesktop/src/common/app/interface.ts
+++ template/vuedesktop/src/common/app/interface.ts
@@ -1,41 +1,46 @@
 import {
-  showDialog,
-  showConfirmDialog,
-  DialogOptions,
-  showToast,
-  ToastOptions,
-} from 'vant'
+  ElMessageBox,
+  ElMessageBoxOptions,
+  ElMessage,
+  MessageOptions,
+} from 'element-plus'
 
-export type AlertProps = string | DialogOptions
+export type AlertProps = string | ElMessageBoxOptions
 
 export const alert = (opts: AlertProps) => {
   if (typeof opts === 'string') {
-    return showDialog({
-      confirmButtonText: '确定',
-      message: opts,
+    return ElMessageBox.alert(opts, '提示', {
+      confirmButtonText: '确定'
     })
   } else {
-    return showDialog(opts)
+    return ElMessageBox.alert(opts.message, opts.title || '提示', Object.assign({
+      confirmButtonText: '确定'
+    }, opts))
   }
 }
 
-export type ConfirmProps = string | DialogOptions
+export type ConfirmProps = string | ElMessageBoxOptions
 
 export const confirm = (opts: ConfirmProps) => {
   if (typeof opts === 'string') {
-    return showConfirmDialog({
-      confirmButtonText: '确定',
-      message: opts,
-    })
+    return ElMessageBox.confirm(opts, '提示')
   } else {
-    return showConfirmDialog(opts)
+    return ElMessageBox.confirm(opts.message, opts.title || '提示', Object.assign({
+      confirmButtonText: '确定',
+      cancelButtonText: '取消'
+    }, opts))
   }
 }
 
-export type ToastProps = string | ToastOptions
+export type ToastProps = MessageOptions
 
 export const toast = (opts: ToastProps) => {
-  showToast(opts)
+  return new Promise((reslove: any) => {
+    ElMessage(Object.assign(opts, {
+      onClose: reslove
+    }))
+  })
+
 }
 
 export default {
