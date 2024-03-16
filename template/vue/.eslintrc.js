009ecdde155f1c33ab4f9f497a085e78f92a9892
dreamjser <dreamjser@gmail.com>
2024-03-17 00:03:31 +0800

    fix: eslint

--- template/vue/.eslintrc.js
+++ template/vue/.eslintrc.js
@@ -34,6 +34,6 @@ module.exports = {
       },
     ],
     'vue/multi-word-component-names': 'off',
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   }
 }
